4c1e1a8565e1a615658d10170ef0c836c6206e50
Michael Wagner <m.t.w@gmx.net>
2022-09-15 08:56:36 +0200

    working drops

--- exts/ai.synctwin.parts_dropper_lite/ai/synctwin/parts_dropper_lite/extension.py
+++ exts/ai.synctwin.parts_dropper_lite/ai/synctwin/parts_dropper_lite/extension.py
@@ -5,6 +5,7 @@ import omni.usd as usd
 from omni.kit.window.filepicker import FilePickerDialog
 from omni.kit.widget.filebrowser import FileBrowserItem
 from .part_dropper import PartDropper
+import omni.physx
 import os 
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
@@ -24,17 +25,17 @@ class PartsDropperLite(omni.ext.IExt):
         self._dropper.set_container_usd(path)
         self._container_label.text = os.path.split(path)[1]
         self._container_size_label.text = f"Size: {self._vec_to_str(self._dropper.container_size)}"
+        self._container_button.enabled = self._dropper.can_create_container()
+        if self._dropper.can_create_container():
+            self.create_container()
+        
 
     def on_container_file_selected(self, dialog, dirname: str, filename: str):        
         print(f"selected {filename}")
         self.set_container_usd(f"{dirname}/{filename}")
         dialog.hide()
 
-    def on_part_file_selected(self, dialog, dirname: str, filename: str):        
-        print(f"selected {filename}")
-        self._dropper.set_part_usd(f"{dirname}/{filename}")
-
-        dialog.hide()
+    
     
     def select_container(self):
         dialog = FilePickerDialog(
@@ -45,7 +46,23 @@ class PartsDropperLite(omni.ext.IExt):
             file_extension_options=DEFAULT_FILE_EXTENSION_TYPES
         )
         dialog.show()
-        
+
+
+    def set_part_usd(self, path):
+        self._dropper.set_part_usd(path)        
+        self._part_label.text = os.path.split(path)[1]
+        self._part_size_label.text = f"Size: {self._vec_to_str(self._dropper.part_size)}"
+        self._parts_button.enabled = self._dropper.can_create_parts()
+        if self._dropper.can_create_parts():
+            self.create_parts()
+       
+
+    def on_part_file_selected(self, dialog, dirname: str, filename: str):        
+        print(f"selected {filename}")
+        self.set_part_usd(f"{dirname}/{filename}")
+
+        dialog.hide()    
+
     def select_part(self):
         dialog = FilePickerDialog(
             "select part geometry",
@@ -57,10 +74,18 @@ class PartsDropperLite(omni.ext.IExt):
         dialog.show()
     
     def on_startup(self, ext_id):
-        self._window = ui.Window("SyncTwin Parts Dropper Lite", width=300, height=300)
+        self._window = ui.Window("SyncTwin Parts Dropper Lite", width=300, height=400)
         self._dropper = PartDropper()
+        self._physx = omni.physx.get_physx_interface()
+        self._app = omni.kit.app.get_app_interface()
+        self._drop_interval_ms = 500
+        #self._physx_cooking = omni.physx.get_physx_cooking_interface()
+        #self._physx_authoring = omni.physx.get_physx_authoring_interface()
+
+        self._is_dropping = False 
         with self._window.frame:
             with ui.VStack():
+                ui.Button("create scene", clicked_fn=lambda: self.create_scene())
                 ui.Label("Container:")
                 with ui.HStack():                    
                     self._container_label = ui.Label("[select container]", height=30)                    
@@ -72,6 +97,8 @@ class PartsDropperLite(omni.ext.IExt):
                             clicked_fn=lambda: self.select_container()
                         )
                 self._container_size_label = ui.Label("[container size]", height=30)                        
+                self._container_button = ui.Button("create container", clicked_fn=lambda: self.create_container(), enabled=False)
+
                 ui.Label("Part:")
                 with ui.HStack():                    
                     self._part_label = ui.Label("[select part]", height=30)
@@ -87,16 +114,65 @@ class PartsDropperLite(omni.ext.IExt):
                 ui.Label("Count:")
                 self._countModel = ui.IntField().model
                 self._countModel.set_value(10)
+                self._parts_button = ui.Button("create parts", clicked_fn=lambda: self.create_parts(), enabled=False)
 
-                
+        # timeline 
+        self._timeline = omni.timeline.get_timeline_interface()
+        timeline_stream = self._timeline.get_timeline_event_stream()
+        self._timeline_event_sub = timeline_stream.create_subscription_to_pop(self._on_timeline_event)                
 
-                ui.Button("create scene", clicked_fn=lambda: self.create_scene())
-    
+         # setup app update subscription for frame events
+        self._app = omni.kit.app.get_app_interface()
+        self._app_update_sub = self._app.get_update_event_stream().create_subscription_to_pop(
+            self._on_app_update_event, name="part_dropper._on_app_update_event"
+        )                
+                    
     def on_shutdown(self):
         print("[ai.synctwin.parts_dropper_lite] MyExtension shutdown")
+        self._timeline_event_sub = None
 
     def create_scene(self):
         context = omni.usd.get_context()
         context.new_stage()
         stage = context.get_stage()
         self._dropper.create_ground_plane(stage)
+
+    def create_container(self):
+        context = omni.usd.get_context()
+        stage = context.get_stage()
+        self._dropper.create_container(stage)
+
+    def create_parts(self):
+        if self._is_dropping:
+            self._is_dropping = False
+            self._timeline.stop()
+            return 
+        context = omni.usd.get_context()
+        stage = context.get_stage()
+        self._last_drop_time_ms = self._app.get_time_since_start_ms()
+        self._dropper.add_part(stage)
+        print("create parts")
+        self._timeline.play()
+        self._is_dropping= True          
+
+        
+    def _on_timeline_event(self, e):
+        """ Event handler for timeline events"""
+        print("timeline event")
+        if e.type == int(omni.timeline.TimelineEventType.PLAY):
+            print("PLAY")
+
+    def _on_app_update_event(self, evt):
+        """ Event handler app update events occuring every frame"""
+        #print("app event")
+        if not self._is_dropping:
+            return 
+        now_ms = self._app.get_time_since_start_ms()
+        elapsed_ms = now_ms - self._last_drop_time_ms
+        #print("elapsed: {elapsed_s}")
+        if elapsed_ms > self._drop_interval_ms:
+            context = omni.usd.get_context()
+            stage = context.get_stage()
+            self._dropper.add_part(stage)
+            self._last_drop_time_s = now_ms
+    
--- exts/ai.synctwin.parts_dropper_lite/ai/synctwin/parts_dropper_lite/part_dropper.py
+++ exts/ai.synctwin.parts_dropper_lite/ai/synctwin/parts_dropper_lite/part_dropper.py
@@ -6,18 +6,33 @@ from omni.physx.scripts.utils import set_physics_scene_asyncsimrender
 class PartDropper:
     def __init__(self) -> None:
         self._cache = UsdGeom.BBoxCache(Usd.TimeCode.Default(), ['default', 'render'])
+        self.container_path = "" 
+        self.part_path = "" 
+        self.part_size = Gf.Vec3f()
+        self.container_path = ""
+        self.container_size = Gf.Vec3f()
+        self.part_count = 0 
+        
+    def can_create_container(self) -> bool:
+        return self.container_path != ""
 
-    def stage_bounds(self, stage) -> Gf.Range3d:
+    def can_create_parts(self) -> bool:
+        return self.can_create_container() and self.part_path != ""
+
+    def bounds(self, usd_path) -> Gf.Range3d:
+        stage = Usd.Stage.Open(usd_path)
         bounds = self._cache.ComputeWorldBound(stage.GetPrimAtPath("/"))
+        stage = None 
         return bounds.GetRange()
 
     def set_container_usd(self, usd_path):
-        self.container_stage = Usd.Stage.Open(usd_path)
-        self.container_size = self.stage_bounds(self.container_stage).GetSize()
+        self.container_path = usd_path
+        self.container_size = self.bounds(usd_path).GetSize()
+
         
     def set_part_usd(self, usd_path):
-        self.part_stage = Usd.Stage.Open(usd_path)
-        self.part_size = self.stage_bounds(self.part_stage).GetSize()
+        self.part_path = usd_path
+        self.part_size = self.bounds(usd_path).GetSize()
     
     def create_ground_plane(self, stage):
         UsdGeom.SetStageUpAxis(stage, UsdGeom.Tokens.z)
@@ -41,4 +56,31 @@ class PartDropper:
         physxSceneAPI.CreateGpuFoundLostPairsCapacityAttr(256 * 1024 * 2)
         physxSceneAPI.CreateGpuMaxRigidPatchCountAttr(1000000)
 
-        add_ground_plane(stage, "/World/GroundPlane", "Z", 750.0, Gf.Vec3f(0.0, 0.0, 0), Gf.Vec3f(0.5))
+        add_ground_plane(stage, "/World/PartDropper/GroundPlane", "Z", 750.0, Gf.Vec3f(0.0, 0.0, 0), Gf.Vec3f(0.5))
+
+    def create_container(self, stage: Usd.Stage):
+        bbox = self.bounds(self.container_path)
+        pcenter = bbox.GetMidpoint()
+        pmin = bbox.GetMin()
+        psize = bbox.GetSize()
+        
+        stage.RemovePrim("/World/PartDropper/Container")
+        box_prim = stage.DefinePrim("/World/PartDropper/Container") 
+        box_prim.GetReferences().AddReference(self.container_path)
+        self._box_offset = Gf.Vec3f(-pcenter[0], -pcenter[1], -pmin[2])
+        UsdGeom.Xformable(box_prim).AddTranslateOp().Set(self._box_offset)
+
+        UsdPhysics.CollisionAPI.Apply(box_prim)
+        
+        mesh_api = UsdPhysics.MeshCollisionAPI.Apply(box_prim)
+
+    def add_part(self, stage: Usd.Stage):
+        self.part_count += 1 
+        
+        part_prim = stage.DefinePrim(f"/World/Box/Parts/Part_{self.part_count}", "Xform")
+        part_prim.GetReferences().AddReference(self.part_path)
+        
+        UsdPhysics.CollisionAPI.Apply(part_prim) 
+        physicsAPI = UsdPhysics.RigidBodyAPI.Apply(part_prim)        
+        
+        UsdGeom.Xformable(part_prim).AddTranslateOp().Set(Gf.Vec3f(0, 0, 50))
